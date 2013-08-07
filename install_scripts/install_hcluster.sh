42ca009d6eded6f181517e65cc59253488a49ae3
dlovell <dlovell@gmail.com>
2013-08-07 14:18:18 -0700

    clean up virtualenv installation process

---
+++ install_scripts/install_hcluster.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+
+# settings
+WORK_DIR=/tmp/
+WHICH_HCLUSTER=0.2.0
+
+
+grep_hcluster=$(yolk | grep hcluster)
+if [[ -z $grep_hcluster ]]; then
+    pip install --download $WORK_DIR hcluster==$WHICH_HCLUSTER
+    cd $WORK_DIR
+    tar xvfz hcluster-$WHICH_HCLUSTER.tar.gz
+    cd hcluster-$WHICH_CLUSTER
+    perl -pi.bak -e "s/input\('Selection \[default=1\]:'\)/2/" setup.py
+    python setup.py install
+fi
+
