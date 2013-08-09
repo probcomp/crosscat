35c6caee2bcc445e764cbf4a9523ba4057670daa
dlovell <dlovell@gmail.com>
2013-08-09 08:05:08 -0700

    BUGFIX: fix typos

--- install_scripts/install_hcluster.sh
+++ install_scripts/install_hcluster.sh
@@ -3,14 +3,14 @@
 
 # settings
 WORK_DIR=/tmp/
-WHICH_HCLUSTER=0.2.0
+WHICH_CLUSTER=0.2.0
 
 
 grep_hcluster=$(yolk | grep hcluster)
 if [[ -z $grep_hcluster ]]; then
-    pip install --download $WORK_DIR hcluster==$WHICH_HCLUSTER
+    pip install --download $WORK_DIR hcluster==$WHICH_CLUSTER
     cd $WORK_DIR
-    tar xvfz hcluster-$WHICH_HCLUSTER.tar.gz
+    tar xvfz hcluster-$WHICH_CLUSTER.tar.gz
     cd hcluster-$WHICH_CLUSTER
     perl -pi.bak -e "s/input\('Selection \[default=1\]:'\)/2/" setup.py
     python setup.py install
