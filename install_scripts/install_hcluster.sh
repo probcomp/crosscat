103ea814d084fe9c2bd5f648a4a578c3f75ae2b9
dlovell <dlovell@gmail.com>
2013-09-14 00:38:37 -0400

    make cached packages dir settable

--- install_scripts/install_hcluster.sh
+++ install_scripts/install_hcluster.sh
@@ -1,17 +1,43 @@
 #!/usr/bin/env bash
 
 
-# settings
-WORK_DIR=/tmp/
-WHICH_CLUSTER=0.2.0
+# set default values
+CACHED_PACKAGES_DIR=/tmp/CachedPythonPackages
+WHICH_HCLUSTER=0.2.0
 
 
-grep_hcluster=$(yolk | grep hcluster)
+# print script usage
+usage() {
+	cat <<EOF
+usage: $0 options	
+	Set up postgres database
+	OPTIONS:
+
+	-h      Show this message
+	-c      CACHED_PACKAGES_DIR=$CACHED_PACKAGES_DIR
+	-w      WHICH_HCLUSTER=$WHICH_HCLUSTER
+EOF
+exit
+}
+
+
+#Process the arguments
+while getopts hc:w: opt
+do
+	case "$opt" in
+		h) usage;;
+		c) CACHED_PACKAGES_DIR=$OPTARG;;
+		w) WHICH_HCLUSTER=$OPTARG;;
+	esac
+done
+
+
+grep_hcluster=$(yolk -l | grep hcluster)
 if [[ -z $grep_hcluster ]]; then
-    pip install --download $WORK_DIR hcluster==$WHICH_CLUSTER
-    cd $WORK_DIR
-    tar xvfz hcluster-$WHICH_CLUSTER.tar.gz
-    cd hcluster-$WHICH_CLUSTER
+    pip install --no-install --download $CACHED_PACKAGES_DIR hcluster==$WHICH_HCLUSTER
+    cd $CACHED_PACKAGES_DIR
+    tar xvfz hcluster-$WHICH_HCLUSTER.tar.gz
+    cd hcluster-$WHICH_HCLUSTER
     perl -pi.bak -e "s/input\('Selection \[default=1\]:'\)/2/" setup.py
     python setup.py install
 fi
