73dfc79f984d9102445e5e3b9132cd300a007053
dlovell <dlovell@gmail.com>
2013-09-14 10:32:12 -0400

    assume already in project virtualenv for user install

--- install_scripts/install_cx_freeze.sh
+++ install_scripts/install_cx_freeze.sh
@@ -4,7 +4,6 @@
 if [[ "$USER" == "root" ]]; then
 	apt-get install -y mercurial
 else
-	workon crosscat
 	hg clone https://bitbucket.org/anthony_tuininga/cx_freeze
 	cd cx_freeze && python setup.py install
 fi
