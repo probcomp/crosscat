1158f3beebd78ea17858f846f44a06b880967129
dlovell <dlovell@gmail.com>
2013-09-17 12:28:27 -0400

    only try to install cx_freeze if it doesnt already exist

--- scripts/install_scripts/install_cx_freeze.sh
+++ scripts/install_scripts/install_cx_freeze.sh
@@ -4,6 +4,9 @@
 if [[ "$USER" == "root" ]]; then
 	apt-get install -y mercurial
 else
-	hg clone https://bitbucket.org/anthony_tuininga/cx_freeze
-	cd cx_freeze && python setup.py install
+	grep_cx_freeze=$(yolk -l | grep cx_Freeze)
+	if [[ -z $grep_cx_freeze ]]; then
+		hg clone https://bitbucket.org/anthony_tuininga/cx_freeze
+		cd cx_freeze && python setup.py install
+	fi
 fi
