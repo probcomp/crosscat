a965362503c13c30656893d4cb785c8a79c79c71
dlovell <dlovell@gmail.com>
2013-09-22 12:27:41 -0400

    add Avinash Gandhe to list of authors

--- crosscat/CrossCatClient.py
+++ crosscat/CrossCatClient.py
@@ -2,7 +2,7 @@
 #   Copyright (c) 2010-2013, MIT Probabilistic Computing Project
 #
 #   Lead Developers: Dan Lovell and Jay Baxter
-#   Authors: Dan Lovell, Baxter Eaves, Jay Baxter, Vikash Mansinghka
+#   Authors: Dan Lovell, Baxter Eaves, Jay Baxter, Vikash Mansinghka, Avinash Gandhe
 #   Research Leads: Vikash Mansinghka, Patrick Shafto
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
