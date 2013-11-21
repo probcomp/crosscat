f8f5b71d20accdf5a848bd2cbcc7f4494ab37789
dlovell <dlovell@gmail.com>
2013-11-21 17:47:35 -0500

    pick up all packages with packages argument to setup; move cython code to path crosscat.cython_code.X

--- setup.py
+++ setup.py
@@ -77,21 +77,21 @@ State_sources = generate_sources([
 
 # create exts
 ContinuousComponentModel_ext = Extension(
-    "crosscat.ContinuousComponentModel",
+    "crosscat.cython_code.ContinuousComponentModel",
     libraries = ['boost_random'],
     extra_compile_args = [],
     sources=ContinuousComponentModel_sources,
     include_dirs=include_dirs,
     language="c++")
 MultinomialComponentModel_ext = Extension(
-    "crosscat.MultinomialComponentModel",
+    "crosscat.cython_code.MultinomialComponentModel",
     libraries = ['boost_random'],
     extra_compile_args = [],
     sources=MultinomialComponentModel_sources,
     include_dirs=include_dirs,
     language="c++")
 State_ext = Extension(
-    "crosscat.State",
+    "crosscat.cython_code.State",
     libraries = ['boost_random'],
     extra_compile_args = [],
     sources=State_sources,
@@ -104,7 +104,7 @@ ext_modules = [
         State_ext,
         ]
 
-packages = ['crosscat']
+packages = ['crosscat', 'crosscat.utils', 'crosscat.convergence_analysis', 'crosscat.jsonrpc_http']
 setup(
         name='CrossCat',
         version='0.1',
