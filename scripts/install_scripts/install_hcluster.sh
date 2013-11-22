76ada05e23dbf9a08220f648ccad20567967abf5
dlovell <dlovell@gmail.com>
2013-11-21 22:31:17 -0500

    only 'pip install --download' if file doesn't exist

--- scripts/install_scripts/install_hcluster.sh
+++ scripts/install_scripts/install_hcluster.sh
@@ -5,6 +5,7 @@ set -u
 
 CACHED_PACKAGES_DIR=/tmp/CachedPythonPackages
 WHICH_HCLUSTER=0.2.0
+hcluster_tgz=hcluster-$WHICH_HCLUSTER.tar.gz
 MISSING=missing
 grep_hcluster=$MISSING
 
@@ -12,9 +13,11 @@ grep_hcluster=$MISSING
 [ grep_hcluster=$(pip freeze | grep hcluster) ] || true
 if [[ $grep_hcluster = "$MISSING" ]]; then
     mkdir -p $CACHED_PACKAGES_DIR
-    pip install --no-install --download $CACHED_PACKAGES_DIR --exists-action b hcluster==$WHICH_HCLUSTER
+    if [[ ! -f $CACHED_PACKAGES_DIR/$hcluster_tgz ]]; then
+        pip install --no-install --download $CACHED_PACKAGES_DIR hcluster==$WHICH_HCLUSTER
+    fi
     cd $CACHED_PACKAGES_DIR
-    tar xvfz hcluster-$WHICH_HCLUSTER.tar.gz
+    tar xvfz $hcluster_tgz
     cd hcluster-$WHICH_HCLUSTER
     perl -pi.bak -e "s/input\('Selection \[default=1\]:'\)/2/" setup.py
     python setup.py install
