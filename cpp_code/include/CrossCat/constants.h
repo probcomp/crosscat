f0437c8c5050473c5e1876e19df9dfffd0d5d8f6
dlovell <dlovell@gmail.com>
2013-12-13 17:30:03 -0500

    add ORDINAL_DATATYPE
    should be passing list of strs to things like Cluster::init_columns instead of inspecting keys in hyper dict

--- cpp_code/include/CrossCat/constants.h
+++ cpp_code/include/CrossCat/constants.h
@@ -27,6 +27,7 @@ static const int MAX_INT = std::numeric_limits<int>::max();
 static const double NaN = 0./0.;
 static const std::string MULTINOMIAL_DATATYPE = "symmetric_dirichlet_discrete";
 static const std::string CONTINUOUS_DATATYPE = "normal_inverse_gamma";
+static const std::string ORDINAL_DATATYPE = "ordinal";
 static const std::string continuous_key = "nu";
 static const std::string multinomial_key = "dirichlet_alpha";
 static const std::string TOGETHER = "together";
