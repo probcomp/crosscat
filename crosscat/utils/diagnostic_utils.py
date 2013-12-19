5453eb6a1c139ebd8cde49c05863ec890fcd08cc
dlovell <dlovell@gmail.com>
2013-12-19 15:50:31 -0500

    actually implement feature_z statistic

--- crosscat/utils/diagnostic_utils.py
+++ crosscat/utils/diagnostic_utils.py
@@ -30,16 +30,28 @@ def get_mean_test_ll(p_State):
 def get_column_partition_assignments(p_State):
     return p_State.get_X_L()['column_partition']['assignments']
 
-def column_partition_assignments_to_f_z_statistic(column_partition_assignments):
-    # FIXME: actually implement this
-    get_num_views_over_iters = lambda vector: map(len, map(set, vector))
-    intermediate = map(get_num_views_over_iters,
-            column_partition_assignments.T)
-    return numpy.array(intermediate).T
+def column_chain_to_ratio(column_chain_arr, j, i=0):
+    chain_i_j = column_chain_arr[[i, j], :]
+    is_same = numpy.diff(chain_i_j, axis=0)[0] == 0
+    n_chains = len(is_same)
+    is_same_count = sum(is_same)
+    ratio = is_same_count / float(n_chains)
+    return ratio
+
+def column_partition_assignments_to_f_z_statistic(column_partition_assignments,
+        j, i=0):
+    iter_column_chain_arr = column_partition_assignments.transpose((1, 0, 2))
+    helper = lambda column_chain_arr: column_chain_to_ratio(column_chain_arr, j, i)
+    as_list = map(helper, iter_column_chain_arr)
+    return numpy.array(as_list)[:, numpy.newaxis]
 
 def default_reprocess_diagnostics_func(diagnostics_arr_dict):
     column_partition_assignments = diagnostics_arr_dict.pop('column_partition_assignments')
-    f_z_statistic = column_partition_assignments_to_f_z_statistic(column_partition_assignments)
-    diagnostics_arr_dict['f_z_statistic'] = f_z_statistic
+    # column_paritition_assignments are column, iter, chain
+    D = column_partition_assignments.shape[0] - 1
+    f_z_statistic_0_1 = column_partition_assignments_to_f_z_statistic(column_partition_assignments, 1, 0)
+    f_z_statistic_0_D = column_partition_assignments_to_f_z_statistic(column_partition_assignments, D, 0)
+    diagnostics_arr_dict['f_z[0, 1]'] = f_z_statistic_0_1
+    diagnostics_arr_dict['f_z[0, D]'] = f_z_statistic_0_D
     #
     return diagnostics_arr_dict
