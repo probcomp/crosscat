4550833bfdc04005d407f5530274268cc6819ea1
dlovell <dlovell@gmail.com>
2013-12-19 20:42:48 -0500

    'rm -rf crosscat' during install so you always get a fresh copy installed

--- crosscat/starcluster_plugin.py
+++ crosscat/starcluster_plugin.py
@@ -46,6 +46,7 @@ class crosscatSetup(ClusterSetup):
                cmd_strs = [
                        # FIXME: could add an if [[ ! -d crosscat ]]; then ... done
                        # to squelch 'git clone' error messages
+                       'rm -rf crosscat',
                        'git clone https://github.com/mit-probabilistic-computing-project/crosscat.git',
                        'bash crosscat/scripts/install_scripts/install.sh',
                        'python crosscat/setup.py install',
