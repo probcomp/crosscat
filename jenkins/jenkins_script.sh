8e841338d2ce494455c75e269ebf79c190d18292
dlovell <dlovell@gmail.com>
2014-01-13 20:16:20 -0500

    remove old cruft no longer necessary with git plugin

--- jenkins/jenkins_script.sh
+++ jenkins/jenkins_script.sh
@@ -1,61 +1,32 @@
 #!/bin/bash
 
-# default values
-branch="master"
-user="jenkins"
-home_dir="/var/lib/jenkins/"
-project_name="crosscat"
 
-# print script usage
-usage() {
-    cat <<EOF
-usage: $0 options
-    
-    Pull down repo from github and run tests via jenkins
-    
-    OPTIONS:
-    -h      Show this message
-    -p	    project name (default=$project_name)
-    -b      set the branch to run (default=$branch)
-    -u      set the user to install virtualenv under (default=$user)
-    -h      set the dir to install virtualenv under (default=$home_dir)
-EOF
-exit
+# Helper functions
+function abort_on_error () {
+	if [[ $? -ne "0" ]]; then
+		echo FAILED: $1
+		exit 1
+	fi
 }
 
 
-#Process command line arguments
-while getopts hp:b:u:d: opt
-do
-    case "$opt" in
-        h) usage;;
-        p) project_name=$OPTARG;;
-        b) branch=$OPTARG;;
-	u) user=$OPTARG;;
-	d) home_dir=$OPTARG;;
-    esac
-done
-
-echo "jenkins_script.sh:"
-echo "project_name: $project_name"
-echo "user: $user"
-echo "branch: $branch"
-echo "home_dir: $home_dir"
-
+# build
 cd $(dirname $(git rev-parse --git-dir))
 python setup.py install
-if [ $? != 0 ]; then
-	exit 1
-else
-	echo "Build via setup.py passed"
-fi
-#
+abort_on_error "Failed to build via setup.py"
+echo "Build via setup.py passed"
+
+
+# make sure shared libraries are linked
+# FIXME; figure out why jenkins can't see shared libraries without this
 cd $(dirname $(git rev-parse --git-dir))
 cd crosscat/cython_code/
 ln -s /usr/local/lib/python2.7/dist-packages/crosscat/cython_code/State.so || true
 ln -s /usr/local/lib/python2.7/dist-packages/crosscat/cython_code/MultinomialComponentModel.so || true
 ln -s /usr/local/lib/python2.7/dist-packages/crosscat/cython_code/ContinuousComponentModel.so || true
-#
+
+
+# run the tests
 cd $(dirname $(git rev-parse --git-dir))
 cd crosscat/tests/unit_tests
 nosetests --with-xunit
