781b3eab4d378481b4c1acffc40b3c5e12a38cd7
dlovell <dlovell@gmail.com>
2014-01-13 20:53:31 -0500

    push long jenkins cli commands into functions

--- jenkins/setup_jenkins.sh
+++ jenkins/setup_jenkins.sh
@@ -34,9 +34,34 @@ do
 	esac
 done
 
-# set derived variables
-jenkins_project=${jenkins_home}/workspace/$project_name
-source_dir=$project_name
+
+# Helper functions
+function install_jenkins_plugin () {
+	plugin_name=$1
+	plugin_version=$2
+	base_url=http://updates.jenkins-ci.org/download/plugins/
+	full_url=$base_url/$plugin_name/$plugin_version/${plugin_name}.hpi
+	jar_dir=/var/cache/jenkins/war/WEB-INF/
+	plugin_dir=${jar_dir}/plugins/
+	#
+	rm ${plugin_dir}/${plugin_name}.hpi
+	java -jar ${jar_dir}/jenkins-cli.jar -s http://127.0.0.1:8080/ install-plugin $full_url
+}
+
+function restart_jenkins () {
+	jar_dir=/var/cache/jenkins/war/WEB-INF/
+	java -jar ${jar_dir}/jenkins-cli.jar -s http://127.0.0.1:8080/ safe-restart
+}
+
+function wait_for_jenkins_to_respond () {
+	wget http://127.0.0.1:8080 2>/dev/null 1>/dev/null
+	while [ $? -ne "0" ]
+	do
+		sleep 2
+		wget http://127.0.0.1:8080 2>/dev/null 1>/dev/null
+	done
+}
+
 
 # install jenkins
 #   per http://pkg.jenkins-ci.org/debian-stable/
@@ -58,18 +83,19 @@ chown -R jenkins $jenkins_home
 bash crosscat/scripts/install_scripts/setup_password_login.sh -u jenkins -p bigdata
 
 # make sure jenkins can install python packages
-install_dir=/usr/local/lib/python2.7/dist-packages
-chown -R jenkins $install_dir
+python_dir=/usr/local/lib/python2.7/dist-packages
+chown -R jenkins $python_dir
 
 
-# give jenkins some time to come up
-sleep 60
-#
-cd /var/cache/jenkins/war/WEB-INF/
-rm plugins/credentials.hpi
-java -jar jenkins-cli.jar -s http://127.0.0.1:8080/ install-plugin http://updates.jenkins-ci.org/download/plugins/scm-api/0.1/scm-api.hpi
-java -jar jenkins-cli.jar -s http://127.0.0.1:8080/ install-plugin http://updates.jenkins-ci.org/download/plugins/credentials/1.9.3/credentials.hpi
-java -jar jenkins-cli.jar -s http://127.0.0.1:8080/ install-plugin http://updates.jenkins-ci.org/download/plugins/ssh-credentials/1.5.1/ssh-credentials.hpi
-java -jar jenkins-cli.jar -s http://127.0.0.1:8080/ install-plugin http://updates.jenkins-ci.org/download/plugins/git-client/1.6.0/git-client.hpi
-java -jar jenkins-cli.jar -s http://127.0.0.1:8080/ install-plugin http://updates.jenkins-ci.org/download/plugins/git/2.0.1/git.hpi
-java -jar jenkins-cli.jar -s http://127.0.0.1:8080/ safe-restart
+# wait for jenkins to respond before proceeding
+wait_for_jenkins_to_respond
+
+#  install plugins and restart
+install_jenkins_plugin scm-api 0.1
+install_jenkins_plugin credentials 1.9.3
+install_jenkins_plugin ssh-credentials 1.5.1
+install_jenkins_plugin git-client 1.6.0
+install_jenkins_plugin git 2.0.1
+install_jenkins_plugin github-api 1.44
+install_jenkins_plugin github 1.8
+restart_jenkins
