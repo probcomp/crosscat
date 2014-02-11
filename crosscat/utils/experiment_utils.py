ccf362581bd66f113f20181174455941cf9c3025
dlovell <dlovell@gmail.com>
2014-02-10 20:42:00 -0500

    refactor
    enable simple experiment running

--- crosscat/utils/experiment_utils.py
+++ crosscat/utils/experiment_utils.py
@@ -7,13 +7,21 @@ import crosscat.utils.general_utils as general_utils
 
 
 result_filename = geweke_utils.summary_filename
+generate_directory_name = geweke_utils.generate_directory_name
+writer = geweke_utils.write_result
+reader = geweke_utils.read_result
+runner = geweke_utils.run_geweke
 
 
 def find_configs(dirname, filename=result_filename):
     root_has_filename = lambda (root, ds, filenames): filenames.count(filename)
     get_filepath = lambda (root, ds, fs): os.path.join(root, filename)
+    def is_one_deep(filepath):
+        _dir, _file = os.path.split(filepath)
+        return os.path.split(_dir)[0] == dirname
     tuples = filter(root_has_filename, os.walk(dirname))
     filepaths = map(get_filepath, tuples)
+    filepaths = filter(is_one_deep, filepaths)
     return filepaths
 
 def read_all_configs(dirname='.'):
@@ -25,35 +33,41 @@ def read_all_configs(dirname='.'):
     config_list = map(read_config, filepaths)
     return config_list
 
-def generate_filepath(config):
-    _dirname = geweke_utils.generate_directory_name(config)
-    filepath = os.path.join(_dirname, result_filename)
-    return filepath
-
 def read_results(config_list, dirname=''):
-    def read_result(config):
-        filepath = generate_filepath(config)
-        result = file_utils.unpickle(filepath, dir=dirname)
-        return result
+    _read_result = lambda config: reader(config, dirname)
     config_list = general_utils.ensure_listlike(config_list)
-    results = map(read_result, config_list)
+    results = map(_read_result, config_list)
     return results
 
-def write_result(config, result, dirname=''):
-    filepath = generate_filepath(config)
-    file_utils.pickle(result, filepath, dirname=dirname)
+def write_results(results, dirname=''):
+    _write_result = lambda result: writer(result, dirname)
+    map(_write_result, results)
     return
 
-def do_experiments(runner, writer, config_list):
+def do_experiments(runner, writer, config_list, dirname=''):
     def do_experiment(config):
-        result = runner(**config)
-        writer(config, result)
-        return result
+        result = runner(config)
+        writer(result, dirname)
+        return
     config_list = general_utils.ensure_listlike(config_list)
-    results = map(do_experiment, config_list)
-    return results
+    map(do_experiment, config_list)
+    return
+
+def args_to_config(args):
+    parser = geweke_utils.generate_parser()
+    args = parser.parse_args(args)
+    args = geweke_utils.arbitrate_args(args)
+    return args.__dict__
 
 if __name__ == '__main__':
+    args_list = [
+            ['--num_rows', '10', '--num_cols', '2', '--num_iters', '300', ],
+            ['--num_rows', '10', '--num_cols', '3', '--num_iters', '300', ],
+            ['--num_rows', '20', '--num_cols', '2', '--num_iters', '300', ],
+            ['--num_rows', '20', '--num_cols', '3', '--num_iters', '300', ],
+            ]
+    configs_list = map(args_to_config, args_list)
+    do_experiments(runner, writer, configs_list)
+
     config_list = read_all_configs()
     results = read_results(config_list[:-1])
-
