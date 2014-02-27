429243153ea8356e68201b819cb78bf82e6552dd
dlovell <dlovell@gmail.com>
2014-02-27 14:29:46 -0500

    fix indentation with autopep

--- crosscat/starcluster_plugin.py
+++ crosscat/starcluster_plugin.py
@@ -27,42 +27,43 @@ from starcluster.logger import log
 # maybe should prefix the command with "source /etc/profile"
 # as starclusters' sshutils.ssh.execute(..., source_profile=True) does
 def execute_as_user(node, user, command_str, **kwargs):
-     cmd_str = 'sudo -H -u %s %s'
-     cmd_str %= (user, command_str)
-     node.ssh.execute(cmd_str, **kwargs)
+    cmd_str = 'sudo -H -u %s %s'
+    cmd_str %= (user, command_str)
+    node.ssh.execute(cmd_str, **kwargs)
 
 
 class crosscatSetup(ClusterSetup):
-     def __init__(self):
-         # TODO: Could be generalized to "install a python package plugin"
-         pass
 
-     def run(self, nodes, master, user, user_shell, volumes):
-          # NOTE: nodes includes master
-         for node in nodes:
-               log.info("Installing CrossCat as root on %s" % node.alias)
-               #
-               # FIXME: should be capturing out, err from script executions
-               cmd_strs = [
-                       # FIXME: could add an if [[ ! -d crosscat ]]; then ... done
-                       # to squelch 'git clone' error messages
-                       'rm -rf crosscat',
-                       'git clone https://github.com/mit-probabilistic-computing-project/crosscat.git',
-                       'bash crosscat/scripts/install_scripts/install.sh',
-                       'python crosscat/setup.py install',
-                       ]
-               for cmd_str in cmd_strs:
-                   node.ssh.execute(cmd_str)
-         for node in nodes:
-               log.info("Setting up CrossCat as user on %s" % node.alias)
-               #
-               cmd_strs = [
-                   'mkdir -p ~/.matplotlib',
-                   'echo backend: Agg > ~/.matplotlib/matplotlibrc',
-                    ]
-               for cmd_str in cmd_strs:
-                   cmd_str = 'bash -c "source /etc/profile && %s"' % cmd_str
-                   execute_as_user(node, user, cmd_str)
+    def __init__(self):
+        # TODO: Could be generalized to "install a python package plugin"
+        pass
+
+    def run(self, nodes, master, user, user_shell, volumes):
+         # NOTE: nodes includes master
+        for node in nodes:
+            log.info("Installing CrossCat as root on %s" % node.alias)
+            #
+            # FIXME: should be capturing out, err from script executions
+            cmd_strs = [
+                # FIXME: could add an if [[ ! -d crosscat ]]; then ... done
+                # to squelch 'git clone' error messages
+                'rm -rf crosscat',
+                'git clone https://github.com/mit-probabilistic-computing-project/crosscat.git',
+                'bash crosscat/scripts/install_scripts/install.sh',
+                'python crosscat/setup.py install',
+            ]
+            for cmd_str in cmd_strs:
+                node.ssh.execute(cmd_str)
+        for node in nodes:
+            log.info("Setting up CrossCat as user on %s" % node.alias)
+            #
+            cmd_strs = [
+                'mkdir -p ~/.matplotlib',
+                'echo backend: Agg > ~/.matplotlib/matplotlibrc',
+            ]
+            for cmd_str in cmd_strs:
+                cmd_str = 'bash -c "source /etc/profile && %s"' % cmd_str
+                execute_as_user(node, user, cmd_str)
 #               # run server
 #               cmd_str = 'bash -i %s'
 #               cmd_str %= S.path.run_server_script.replace(S.path.this_repo_dir, S.path.remote_code_dir)
