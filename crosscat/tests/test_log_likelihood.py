4f3822484a4d1d72cfd163493b33d05f1e2d1f8e
dlovell <dlovell@gmail.com>
2014-03-06 19:10:03 -0500

    refactor into experiment_runner like functions

--- crosscat/tests/test_log_likelihood.py
+++ crosscat/tests/test_log_likelihood.py
@@ -1,4 +1,5 @@
 import argparse
+import random
 from functools import partial
 #
 import numpy
@@ -13,51 +14,85 @@ import crosscat.utils.timing_test_utils as ttu
 import crosscat.utils.convergence_test_utils as ctu
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--gen_seed', default=0, type=int)
-parser.add_argument('--num_rows', default=100, type=int)
-parser.add_argument('--num_cols', default=4, type=int)
-parser.add_argument('--num_clusters', default=5, type=int)
-parser.add_argument('--num_views', default=1, type=int)
-parser.add_argument('--n_steps', default=10, type=int)
-args = parser.parse_args()
-#
-gen_seed = args.gen_seed
-num_rows = args.num_rows
-num_cols = args.num_cols
-num_clusters = args.num_clusters
-num_views = args.num_views
-n_steps = args.n_steps
-#
-n_test = num_rows / 10
+def generate_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gen_seed', default=0, type=int)
+    parser.add_argument('--num_rows', default=100, type=int)
+    parser.add_argument('--num_cols', default=4, type=int)
+    parser.add_argument('--num_clusters', default=5, type=int)
+    parser.add_argument('--num_views', default=1, type=int)
+    parser.add_argument('--n_steps', default=10, type=int)
+    parser.add_argument('--n_test', default=None, type=int)
+    return parser
 
+def arbitrate_args(args):
+    if args.n_test is None:
+        args.n_test = args.num_rows / 10
+    return args
 
-# generate data
-T, M_c, M_r, gen_X_L, gen_X_D = ttu.generate_clean_state(gen_seed, num_clusters,
-        num_cols, num_rows, num_views)
-engine = LocalEngine()
-T_test = gu.sample_T(engine, M_c, T, gen_X_L, gen_X_D)
-X_L, X_D = engine.initialize(M_c, M_r, T)
-gen_mtll = ctu.calc_mean_test_log_likelihood(M_c, T, gen_X_L, gen_X_D, T_test)
-gen_preplexity = ctu.calc_mean_test_log_likelihood(M_c, T, gen_X_L, gen_X_D, T)
+def args_to_config(args):
+    parser = generate_parser()
+    args = parser.parse_args(args)
+    args = arbitrate_args(args)
+    config = args.__dict__
+    return config
 
+def test_log_likelihood_quality_test(config):
+    gen_seed = config['gen_seed']
+    num_rows = config['num_rows']
+    num_cols = config['num_cols']
+    num_clusters = config['num_clusters']
+    num_views = config['num_views']
+    n_steps = config['n_steps']
+    n_test = config['n_test']
 
-# run inference
-calc_perplexity = lambda p_State: \
-        numpy.mean(map(p_State.calc_row_predictive_logp, T))
-calc_test_log_likelihood = lambda p_State: \
-        numpy.mean(map(p_State.calc_row_predictive_logp, T_test))
-#
-diagnostic_func_dict = dict(
-        perplexity=calc_perplexity,
-        test_log_likelihood=calc_test_log_likelihood,
-        )
-X_L, X_D, diagnostics_dict = engine.analyze(M_c, T, X_L, X_D,
-        do_diagnostics=diagnostic_func_dict, n_steps=n_steps)
+    # generate data
+    T, M_c, M_r, gen_X_L, gen_X_D = ttu.generate_clean_state(gen_seed, num_clusters,
+            num_cols, num_rows, num_views)
+    engine = LocalEngine()
+    sampled_T = gu.sample_T(engine, M_c, T, gen_X_L, gen_X_D)
+    T_test = random.sample(sampled_T, n_test)
+    gen_data_ll = ctu.calc_mean_test_log_likelihood(M_c, T, gen_X_L, gen_X_D, T)
+    gen_test_set_ll = ctu.calc_mean_test_log_likelihood(M_c, T, gen_X_L, gen_X_D, T_test)
 
+    # run inference
+    def calc_ll(T, p_State):
+        log_likelihoods = map(p_State.calc_row_predictive_logp, T)
+        mean_log_likelihood = numpy.mean(log_likelihoods)
+        return mean_log_likelihood
+    calc_data_ll = partial(calc_ll, T)
+    calc_test_set_ll = partial(calc_ll, T_test)
+    diagnostic_func_dict = dict(
+            data_ll=calc_data_ll,
+            test_set_ll=calc_test_set_ll,
+            )
+    X_L, X_D = engine.initialize(M_c, M_r, T)
+    X_L, X_D, diagnostics_dict = engine.analyze(M_c, T, X_L, X_D,
+            do_diagnostics=diagnostic_func_dict, n_steps=n_steps)
 
-# plot
-pylab.plot(diagnostics_dict['test_log_likelihood'], 'g')
-pylab.plot(diagnostics_dict['perplexity'], 'r')
-pylab.axhline(gen_mtll, color='k')
-pylab.axhline(gen_preplexity, color='b')
+    result = dict(
+            diagnostics_dict=diagnostics_dict,
+            gen_data_ll=gen_data_ll,
+            gen_test_set_ll=gen_test_set_ll,
+            )
+    return result
+
+def plot_result(result):
+    diagnostics_dict = result['diagnostics_dict']
+    gen_data_ll = result['gen_data_ll']
+    gen_test_set_ll = result['gen_test_set_ll']
+    #
+    pylab.plot(diagnostics_dict['data_ll'], 'g')
+    pylab.plot(diagnostics_dict['test_set_ll'], 'r')
+    pylab.axhline(gen_data_ll, color='g', linestyle='--')
+    pylab.axhline(gen_test_set_ll, color='r', linestyle='--')
+    return
+
+if __name__ == '__main__':
+    parser = generate_parser()
+    args = parser.parse_args()
+    args = arbitrate_args(args)
+    config = args.__dict__
+
+    result = test_log_likelihood_quality_test(config)
+    plot_result(result)
