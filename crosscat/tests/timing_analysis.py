7125a7276bb7ef8295965e9f6ab6cc0e5a255d90
dlovell <dlovell@gmail.com>
2014-03-08 15:33:20 -0500

    account for changes to timing_test_utils

--- crosscat/tests/timing_analysis.py
+++ crosscat/tests/timing_analysis.py
@@ -1,19 +1,14 @@
 import argparse
-#
-import crosscat.utils.timing_test_utils as ttu
-from crosscat.utils.general_utils import Timer, MapperContext, NoDaemonPool
-import experiment_runner.experiment_utils as experiment_utils
 
 
-default_num_rows = [100, 400, 1000, 4000]
-default_num_cols = [8, 16, 32]
-default_num_clusters = [1, 2]
-default_num_views = [1, 2]
-
-def generate_parser():
+def _generate_parser():
+    default_num_rows = [100, 400, 1000, 4000]
+    default_num_cols = [8, 16, 32]
+    default_num_clusters = [1, 2]
+    default_num_views = [1, 2]
+    #
     parser = argparse.ArgumentParser()
     parser.add_argument('--dirname', default='timing_analysis', type=str)
-    parser.add_argument('--plot_prefix', default=None, type=str)
     parser.add_argument('--num_rows', nargs='+', default=default_num_rows, type=int)
     parser.add_argument('--num_cols', nargs='+', default=default_num_cols, type=int)
     parser.add_argument('--num_clusters', nargs='+', default=default_num_clusters, type=int)
@@ -21,34 +16,35 @@ def generate_parser():
     parser.add_argument('--no_plots', action='store_true')
     return parser
 
+def _munge_args(args):
+    kwargs = args.__dict__.copy()
+    dirname = kwargs.pop('dirname')
+    generate_plots = not kwargs.pop('no_plots')
+    return kwargs, dirname, generate_plots
+
 
 if __name__ == '__main__':
-    parser = generate_parser()
-    args = parser.parse_args()
-    args = args.__dict__
-    dirname = args.pop('dirname')
-    plot_prefix = args.pop('plot_prefix')
-    generate_plots = not args.pop('no_plots')
+    from crosscat.utils.general_utils import Timer, MapperContext, NoDaemonPool
+    from crosscat.utils.timing_test_utils import reader, read_all_configs, \
+            read_results, writer, runner, gen_configs
+    import crosscat.utils.timing_test_utils as ttu
+    import experiment_runner.experiment_utils as eu
 
-    is_result_filepath = ttu.is_result_filepath
-    config_to_filepath = ttu.config_to_filepath
-    runner = ttu.runner
-    #
-    do_experiments = experiment_utils.do_experiments
-    writer = experiment_utils.get_fs_writer(config_to_filepath)
-    read_all_configs, reader, read_results = experiment_utils.get_fs_reader_funcs(
-            is_result_filepath, config_to_filepath)
+    # parse args
+    parser = _generate_parser()
+    args = parser.parse_args()
+    kwargs, dirname, generate_plots = _munge_args(args)
 
 
     config_list = ttu.gen_configs(
             kernel_list = ttu._kernel_list,
             n_steps=[10],
-            **args
+            **kwargs
             )
     with Timer('experiments') as timer:
         with MapperContext(Pool=NoDaemonPool) as mapper:
             # use non-daemonic mapper since run_geweke spawns daemonic processes
-            do_experiments(config_list, runner, writer, dirname, mapper)
+            eu.do_experiments(config_list, runner, writer, dirname, mapper)
             pass
         pass
 
@@ -59,11 +55,4 @@ if __name__ == '__main__':
         _all_results = read_results(all_configs, dirname)
         is_same_shape = lambda result: result['start_dims'] == result['end_dims']
         use_results = filter(is_same_shape, _all_results)
-        results_frame = experiment_utils.results_to_frame(use_results)
-        # generate each type of plot
-        filter_join = lambda join_with, list: join_with.join(filter(None, list))
-        for vary_what in ['rows', 'cols', 'clusters', 'views']:
-            plot_filename = filter_join('_', [plot_prefix, 'vary', vary_what])
-            ttu.plot_results(use_results, vary_what, plot_filename)
-            pass
-        pass
+        ttu.plot_results(use_results, dirname=dirname)
