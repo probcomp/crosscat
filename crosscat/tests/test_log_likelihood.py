fdf39832cec89970ea30f164acc8c51e3bceb735
dlovell <dlovell@gmail.com>
2014-03-08 20:50:19 -0500

    reduce default experiment size

--- crosscat/tests/test_log_likelihood.py
+++ crosscat/tests/test_log_likelihood.py
@@ -87,9 +87,9 @@ def plotter(result):
 
 def _generate_parser():
     default_gen_seed = [0]
-    default_num_rows = [100, 200, 500, 1000]
+    default_num_rows = [100, 200, 500]
     default_num_cols = [10, 20, 40]
-    default_num_clusters = [1, 10, 50]
+    default_num_clusters = [1, 10, 20]
     default_num_views = [1, 5, 10]
     default_n_steps = [10]
     default_n_test = [20]
