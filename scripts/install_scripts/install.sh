a1c0f18e1a0fc555df58595092ef3ba0651fdfd4
dlovell <dlovell@gmail.com>
2014-07-01 10:40:44 -0400

    dynamically determine which boost to install

--- scripts/install_scripts/install.sh
+++ scripts/install_scripts/install.sh
@@ -9,8 +9,17 @@ if [[ "$USER" != "root" ]]; then
 fi
 
 
+function get_which_boost {
+	this_release=$(lsb_release -r | awk '{print $NF}')
+	if [[ "$this_release" > "12.04" ]]; then
+		echo libboost-all-dev
+	else
+		echo libboost1.48-all-dev
+	fi
+}
 
 
+which_boost=$(get_which_boost)
 
 
 # update seems necessary, else get
@@ -20,10 +29,9 @@ apt-get update
 # engine dependencies
 apt-get build-dep -y python-numpy python-matplotlib python-scipy
 apt-get remove -y python-numpy python-setuptools python-sphinx ipython
-apt-get install -y libboost1.48-all-dev ccache
+apt-get install -y $which_boost ccache
 # doc dependencies
 apt-get install -y doxygen
-
-# 
+# python dependencies
 wget https://bootstrap.pypa.io/get-pip.py -O- | python
 pip install -U distribute
