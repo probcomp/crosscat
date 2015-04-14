d18f832a7cfecf5e1c77443f36002cdb2324af5b
Taylor R Campbell <riastradh+probcomp@csail.mit.edu>
2015-04-14 16:34:04 +0000

    Constify cluster.

--- cpp_code/include/CrossCat/Cluster.h
+++ cpp_code/include/CrossCat/Cluster.h
@@ -39,8 +39,7 @@
 
 class Cluster {
 public:
-    //Cluster(const std::vector<CM_Hypers*> hypers_v);
-    Cluster(std::vector<CM_Hypers*>& hypers_v);
+    Cluster(const std::vector<CM_Hypers*>& hypers_v);
     Cluster();
     //
     // getters
@@ -56,36 +55,36 @@ public:
     // calculators
     std::vector<double> calc_marginal_logps() const;
     double calc_sum_marginal_logps() const ;
-    double calc_row_predictive_logp(std::vector<double> vd) const;
+    double calc_row_predictive_logp(const std::vector<double>& vd) const;
     std::vector<double> calc_hyper_conditionals(int which_col,
-            std::string which_hyper,
-            std::vector<double> hyper_grid) const;
-    double calc_column_predictive_logp(std::vector<double> column_data,
-                                       std::string col_datatype,
-                                       std::vector<int> data_global_row_indices,
-                                       CM_Hypers hypers);
+            const std::string& which_hyper,
+            const std::vector<double>& hyper_grid) const;
+    double calc_column_predictive_logp(const std::vector<double>& column_data,
+                                       const std::string& col_datatype,
+                                       const std::vector<int>& data_global_row_indices,
+                                       const CM_Hypers& hypers);
     //
     // mutators
-    double insert_row(std::vector<double> values, int row_idx);
-    double remove_row(std::vector<double> values, int row_idx);
+    double insert_row(const std::vector<double>& values, int row_idx);
+    double remove_row(const std::vector<double>& values, int row_idx);
     double remove_col(int col_idx);
-    double insert_col(std::vector<double> data,
-                      std::string col_datatype,
-                      std::vector<int> data_global_row_indices,
-                      CM_Hypers& hypers);
+    double insert_col(const std::vector<double>& data,
+                      const std::string& col_datatype,
+                      const std::vector<int>& data_global_row_indices,
+                      const CM_Hypers& hypers);
     double incorporate_hyper_update(int which_col);
     void delete_component_models(bool check_empty = true);
     //
     // helpers
     friend std::ostream& operator<<(std::ostream& os, const Cluster& c);
-    std::string to_string(std::string join_str = "\n",
+    std::string to_string(const std::string& join_str = "\n",
                           bool top_level = false) const;
     //
     // make private later
     std::vector<ComponentModel*> p_model_v;
 private:
     double score;
-    void init_columns(std::vector<CM_Hypers*>& hypers_v);
+    void init_columns(const std::vector<CM_Hypers*>& hypers_v);
     std::set<int> row_indices;
 };
 
