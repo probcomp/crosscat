f06df258462c737723efeb82e0c3c2f1e0faaadf
Baxter Eaves <baxyjr@gmail.com>
2015-06-09 12:34:32 -0400

    tests for multiple dependent or multiple independent constraints in ensure

--- crosscat/tests/test_col_ensure.py
+++ crosscat/tests/test_col_ensure.py
@@ -3,6 +3,7 @@ from crosscat.utils import data_utils as du
 import random
 import time
 import pytest
+import itertools as it
 
 N_COLS = 10
 N_ROWS = 10
@@ -126,7 +127,45 @@ def test_one_col_independency_analyze(quick_le):
     assert engine.assert_col(X_L, X_D, col1, col2, False, True)
 
 
-# FIXME: causes segfault
+def test_multiple_col_dependency_analyze(quick_le):
+    T, M_r, M_c, X_L, X_D, engine = quick_le
+    while len(set(X_L['column_partition']['assignments'])) == N_COLS:
+        X_L, X_D = engine.initialize(M_c, M_r, T)
+    col_pairs = [(c1, c2,) for c1, c2 in it.combinations(range(N_COLS), 2)]
+    independent_pairs = random.sample(col_pairs, 4)
+    relinfo = [(c1, c2, True) for c1, c2 in independent_pairs]
+
+    X_L, X_D = engine.ensure_col(M_c, M_r, T, X_L, X_D, relinfo)
+
+    for col1, col2, dep in relinfo:
+        assert engine.assert_col(X_L, X_D, col1, col2, dep, True)
+
+    X_L, X_D = engine.analyze(M_c, T, X_L, X_D, n_steps=1)
+
+    for col1, col2, dep in relinfo:
+        assert engine.assert_col(X_L, X_D, col1, col2, dep, True)
+
+
+def test_multiple_col_independency_analyze(quick_le):
+    T, M_r, M_c, X_L, X_D, engine = quick_le
+    while len(set(X_L['column_partition']['assignments'])) == N_COLS:
+        X_L, X_D = engine.initialize(M_c, M_r, T)
+    col_pairs = [(c1, c2,) for c1, c2 in it.combinations(range(N_COLS), 2)]
+    independent_pairs = random.sample(col_pairs, 4)
+    relinfo = [(c1, c2, False) for c1, c2 in independent_pairs]
+
+    X_L, X_D = engine.ensure_col(M_c, M_r, T, X_L, X_D, relinfo)
+
+    for col1, col2, dep in relinfo:
+        assert engine.assert_col(X_L, X_D, col1, col2, dep, True)
+
+    X_L, X_D = engine.analyze(M_c, T, X_L, X_D, n_steps=1)
+
+    for col1, col2, dep in relinfo:
+        assert engine.assert_col(X_L, X_D, col1, col2, dep, True)
+
+
+# # FIXME: causes segfault
 # def test_one_of_each_analyze(quick_le):
 #     T, M_r, M_c, X_L, X_D, engine = quick_le
 #     colrange = [i for i in range(N_COLS)]
