48edccf5a3672bd31fff668b518274e16687b894
Baxter Eaves <baxyjr@gmail.com>
2015-06-09 15:08:18 -0400

    Parameterize tests. Explicitly tests seed that causes failures.

--- crosscat/tests/test_col_ensure.py
+++ crosscat/tests/test_col_ensure.py
@@ -1,12 +1,20 @@
 from crosscat import LocalEngine as LE
 from crosscat.utils import data_utils as du
 import random
-import time
+import numpy
 import pytest
 import itertools as it
 
 N_COLS = 10
 N_ROWS = 10
+
+FAILURE_SEED = 130223
+PASS_SEED = 67944
+
+single_args = [item for item in it.product(
+    [FAILURE_SEED, PASS_SEED], [True, False], [True, False])]
+one_each_args = [item for item in it.product(
+    [FAILURE_SEED, PASS_SEED], [True, False])]
 
 
 def first_duplicate(a):
@@ -20,53 +28,47 @@ def first_duplicate(a):
     return None
 
 
-@pytest.fixture
-def quick_le():
-    n_chains = 1
-    seed = int(time.time())
+def quick_le(seed, n_chains=1):
+    random.seed(seed)
+    numpy.random.seed(seed)
     T, M_r, M_c = du.gen_factorial_data_objects(seed, 2, N_COLS, N_ROWS, 2)
     engine = LE.LocalEngine(seed=seed)
     X_L, X_D = engine.initialize(M_c, M_r, T, n_chains=n_chains)
     return T, M_r, M_c, X_L, X_D, engine
 
 
-def test_one_col_dependency_init(quick_le):
-    T, M_r, M_c, X_L, X_D, engine = quick_le
-    while len(set(X_L['column_partition']['assignments'])) == 1:
-        X_L, X_D = engine.initialize(M_c, M_r, T)
-
-    col1 = X_L['column_partition']['assignments'].index(0)
-    col2 = X_L['column_partition']['assignments'].index(1)
-
-    assert X_L['column_partition']['assignments'][col1] != \
-        X_L['column_partition']['assignments'][col2]
+@pytest.mark.parametrize("seed, dependent, analyze", single_args)
+def test_col_ensure_with_single_contraint(seed, dependent, analyze):
+    T, M_r, M_c, X_L, X_D, engine = quick_le(seed)
+    if dependent:
+        while len(set(X_L['column_partition']['assignments'])) == 1:
+            X_L, X_D = engine.initialize(M_c, M_r, T)
+        col1 = X_L['column_partition']['assignments'].index(0)
+        col2 = X_L['column_partition']['assignments'].index(1)
+        assert X_L['column_partition']['assignments'][col1] != \
+            X_L['column_partition']['assignments'][col2]
+    else:
+        while len(set(X_L['column_partition']['assignments'])) == N_COLS:
+            X_L, X_D = engine.initialize(M_c, M_r, T)
+        dup_idx = first_duplicate(X_L['column_partition']['assignments'])
+        col1 = dup_idx[0]
+        col2 = dup_idx[1]
+        assert X_L['column_partition']['assignments'][col1] == \
+            X_L['column_partition']['assignments'][col2]
 
     relinfo = [(col1, col2, True)]
     X_L, X_D = engine.ensure_col(M_c, M_r, T, X_L, X_D, relinfo)
 
     assert engine.assert_col(X_L, X_D, col1, col2, True, True)
 
-
-def test_one_col_independency_init(quick_le):
-    T, M_r, M_c, X_L, X_D, engine = quick_le
-    while len(set(X_L['column_partition']['assignments'])) == N_COLS:
-        X_L, X_D = engine.initialize(M_c, M_r, T)
-
-    dup_idx = first_duplicate(X_L['column_partition']['assignments'])
-    col1 = dup_idx[0]
-    col2 = dup_idx[1]
-
-    assert X_L['column_partition']['assignments'][col1] == \
-        X_L['column_partition']['assignments'][col2]
-
-    relinfo = [(col1, col2, False)]
-    X_L, X_D = engine.ensure_col(M_c, M_r, T, X_L, X_D, relinfo)
-
-    assert engine.assert_col(X_L, X_D, col1, col2, False, True)
+    if analyze:
+        X_L, X_D = engine.analyze(M_c, T, X_L, X_D, n_steps=1)
+        assert engine.assert_col(X_L, X_D, col1, col2, dependent, True)
 
 
-def test_one_of_each_init(quick_le):
-    T, M_r, M_c, X_L, X_D, engine = quick_le
+@pytest.mark.parametrize("seed, analyze", one_each_args)
+def test_one_of_each_init(seed, analyze):
+    T, M_r, M_c, X_L, X_D, engine = quick_le(seed)
     colrange = [i for i in range(N_COLS)]
 
     dep_col1 = random.choice(colrange)
@@ -83,108 +85,27 @@ def test_one_of_each_init(quick_le):
     for col1, col2, dependent in relinfo:
         assert engine.assert_col(X_L, X_D, col1, col2, dependent, True)
 
+    if analyze:
+        X_L, X_D = engine.analyze(M_c, T, X_L, X_D, n_steps=1)
 
-def test_one_col_dependency_analyze(quick_le):
-    T, M_r, M_c, X_L, X_D, engine = quick_le
-    while len(set(X_L['column_partition']['assignments'])) == 1:
-        X_L, X_D = engine.initialize(M_c, M_r, T)
-
-    col1 = X_L['column_partition']['assignments'].index(0)
-    col2 = X_L['column_partition']['assignments'].index(1)
-
-    assert X_L['column_partition']['assignments'][col1] != \
-        X_L['column_partition']['assignments'][col2]
-
-    relinfo = [(col1, col2, True)]
-    X_L, X_D = engine.ensure_col(M_c, M_r, T, X_L, X_D, relinfo)
-
-    assert engine.assert_col(X_L, X_D, col1, col2, True, True)
-
-    X_L, X_D = engine.analyze(M_c, T, X_L, X_D, n_steps=1)
-
-    assert engine.assert_col(X_L, X_D, col1, col2, True, True)
+        for col1, col2, dependent in relinfo:
+            assert engine.assert_col(X_L, X_D, col1, col2, dependent, True)
 
 
-def test_one_col_independency_analyze(quick_le):
-    T, M_r, M_c, X_L, X_D, engine = quick_le
-    while len(set(X_L['column_partition']['assignments'])) == N_COLS:
-        X_L, X_D = engine.initialize(M_c, M_r, T)
-
-    dup_idx = first_duplicate(X_L['column_partition']['assignments'])
-    col1 = dup_idx[0]
-    col2 = dup_idx[1]
-
-    assert X_L['column_partition']['assignments'][col1] == \
-        X_L['column_partition']['assignments'][col2]
-
-    relinfo = [(col1, col2, False)]
-    X_L, X_D = engine.ensure_col(M_c, M_r, T, X_L, X_D, relinfo)
-
-    assert engine.assert_col(X_L, X_D, col1, col2, False, True)
-
-    X_L, X_D = engine.analyze(M_c, T, X_L, X_D, n_steps=1)
-
-    assert engine.assert_col(X_L, X_D, col1, col2, False, True)
-
-
-def test_multiple_col_dependency_analyze(quick_le):
-    T, M_r, M_c, X_L, X_D, engine = quick_le
-    while len(set(X_L['column_partition']['assignments'])) == N_COLS:
-        X_L, X_D = engine.initialize(M_c, M_r, T)
+@pytest.mark.parametrize("seed, dependent, analyze", single_args)
+def test_multiple_col_ensure(seed, dependent, analyze):
+    T, M_r, M_c, X_L, X_D, engine = quick_le(seed)
     col_pairs = [(c1, c2,) for c1, c2 in it.combinations(range(N_COLS), 2)]
-    independent_pairs = random.sample(col_pairs, 4)
-    relinfo = [(c1, c2, True) for c1, c2 in independent_pairs]
+    ensure_pairs = random.sample(col_pairs, 4)
+    relinfo = [(c1, c2, dependent) for c1, c2 in ensure_pairs]
 
     X_L, X_D = engine.ensure_col(M_c, M_r, T, X_L, X_D, relinfo)
 
     for col1, col2, dep in relinfo:
         assert engine.assert_col(X_L, X_D, col1, col2, dep, True)
 
-    X_L, X_D = engine.analyze(M_c, T, X_L, X_D, n_steps=1)
+    if analyze:
+        X_L, X_D = engine.analyze(M_c, T, X_L, X_D, n_steps=1)
 
-    for col1, col2, dep in relinfo:
-        assert engine.assert_col(X_L, X_D, col1, col2, dep, True)
-
-
-def test_multiple_col_independency_analyze(quick_le):
-    T, M_r, M_c, X_L, X_D, engine = quick_le
-    while len(set(X_L['column_partition']['assignments'])) == N_COLS:
-        X_L, X_D = engine.initialize(M_c, M_r, T)
-    col_pairs = [(c1, c2,) for c1, c2 in it.combinations(range(N_COLS), 2)]
-    independent_pairs = random.sample(col_pairs, 4)
-    relinfo = [(c1, c2, False) for c1, c2 in independent_pairs]
-
-    X_L, X_D = engine.ensure_col(M_c, M_r, T, X_L, X_D, relinfo)
-
-    for col1, col2, dep in relinfo:
-        assert engine.assert_col(X_L, X_D, col1, col2, dep, True)
-
-    X_L, X_D = engine.analyze(M_c, T, X_L, X_D, n_steps=1)
-
-    for col1, col2, dep in relinfo:
-        assert engine.assert_col(X_L, X_D, col1, col2, dep, True)
-
-
-# # FIXME: causes segfault
-# def test_one_of_each_analyze(quick_le):
-#     T, M_r, M_c, X_L, X_D, engine = quick_le
-#     colrange = [i for i in range(N_COLS)]
-# 
-#     dep_col1 = random.choice(colrange)
-#     del colrange[colrange.index(dep_col1)]
-#     dep_col2 = random.choice(colrange)
-# 
-#     ind_col1 = random.choice(colrange)
-#     del colrange[colrange.index(ind_col1)]
-#     ind_col2 = random.choice(colrange)
-# 
-#     relinfo = [(dep_col1, dep_col2, True), (ind_col1, ind_col2, False)]
-#     X_L, X_D = engine.ensure_col(M_c, M_r, T, X_L, X_D, relinfo)
-# 
-#     for col1, col2, dependent in relinfo:
-#         assert engine.assert_col(X_L, X_D, col1, col2, dependent, True)
-# 
-#     X_L, X_D = engine.analyze(M_c, T, X_L, X_D, n_steps=1)
-# 
-#     for col1, col2, dependent in relinfo:
-#         assert engine.assert_col(X_L, X_D, col1, col2, dependent, True)
+        for col1, col2, dep in relinfo:
+            assert engine.assert_col(X_L, X_D, col1, col2, dep, True)
