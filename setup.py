cbadce27793a37bf42e563522b70c30d568b3d05
Taylor R Campbell <riastradh+probcomp@csail.mit.edu>
2015-11-18 20:59:21 +0000

    Bump version to 0.1.44 for previous.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
     from distutils.core import setup
 from distutils.extension import Extension
 
-version = '0.1.43'
+version = '0.1.44'
 
 try:
     with open('src/version.py', 'rU') as f:
