e71652fb5f19c94b9d024d632fa5aee9428b3e57
Taylor R Campbell <riastradh+probcomp@csail.mit.edu>
2016-04-05 23:19:12 +0000

    Test Bessel functions at normal grids around 0 and 15.

--- cpp_code/tests/bessel.cpp
+++ cpp_code/tests/bessel.cpp
@@ -20,6 +20,7 @@
 //      c++ -o bessel bessel.cpp
 //      ./bessel
 
+#include <boost/math/distributions/normal.hpp>
 #include <boost/math/special_functions/bessel.hpp>
 #include <cstdio>
 
@@ -28,40 +29,61 @@ int main(int argc, char **argv) {
     const double lo = -709;
     const double hi = +709;
     const double w = (hi - lo)/n;
+    unsigned nu;
     size_t i;
 
     (void)argc;
     (void)argv;
 
-    if (printf("static const double i0e[] = {\n") < 0) {
-        perror("printf");
-        return 1;
-    }
-    for (i = 0; i < n; i++) {
-        const double y = boost::math::cyl_bessel_i(0, lo + i*w);
-        if (printf("    %.17e,\n", y) < 0) {
+    for (nu = 0; nu < 2; nu++) {
+        if (printf("static const double i%ue[][2] = {\n", nu) < 0) {
             perror("printf");
             return 1;
         }
-    }
-    if (printf("};\n") < 0) {
-        perror("printf");
-        return 1;
-    }
-    if (printf("static const double i1e[] = {\n") < 0) {
-        perror("printf");
-        return 1;
-    }
-    for (i = 0; i < n; i++) {
-        const double y = boost::math::cyl_bessel_i(1, lo + i*w);
-        if (printf("    %.17e,\n", y) < 0) {
+        if (printf("    // Uniform [-709, +709] grid\n") < 0) {
             perror("printf");
             return 1;
         }
-    }
-    if (printf("};\n") < 0) {
-        perror("printf");
-        return 1;
+        for (i = 0; i < n; i++) {
+            const double x = lo + i*w;
+            const double y = boost::math::cyl_bessel_i(nu, x);
+            if (printf("    { %.17e, %.17e },\n", x, y) < 0) {
+                perror("printf");
+                return 1;
+            }
+        }
+        if (printf("    // N(0, 1e-4) grid\n") < 0) {
+            perror("printf");
+            return 1;
+        }
+        for (i = 0; i < n; i++) {
+            boost::math::normal norm(0, 1e-4);
+            const double p = static_cast<double>(i + 1)/(n + 2);
+            const double x = boost::math::quantile(norm, p);
+            const double y = boost::math::cyl_bessel_i(nu, x);
+            if (printf("    { %.17e, %.17e },\n", x, y) < 0) {
+                perror("printf");
+                return 1;
+            }
+        }
+        if (printf("    // N(15, 1e-4) grid\n") < 0) {
+            perror("printf");
+            return 1;
+        }
+        for (i = 0; i < n; i++) {
+            boost::math::normal norm(15, 1e-4);
+            const double p = static_cast<double>(i + 1)/(n + 2);
+            const double x = boost::math::quantile(norm, p);
+            const double y = boost::math::cyl_bessel_i(nu, x);
+            if (printf("    { %.17e, %.17e },\n", x, y) < 0) {
+                perror("printf");
+                return 1;
+            }
+        }
+        if (printf("};\n") < 0) {
+            perror("printf");
+            return 1;
+        }
     }
 
     return 0;
